28cd08f97d224d689215ff266765ccc8dbb53641
iowokakomukai <69210735+iowokakomukai@users.noreply.github.com>
2020-09-05 00:25:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -106,6 +106,7 @@ async def on_member_update(before, after):#Member情報変更時に呼び出し
         await channel.send("break")
         #該当する要素が見つかった時点でブレイクします。
         break
+    await channel.send("loop")
   for item in after.roles :
     await channel.send("for")
     if "nvwau" in item :
