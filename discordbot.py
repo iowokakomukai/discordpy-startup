14e0a7a55edf29be4cecf87737f021c1a2052564
iowokakomukai <69210735+iowokakomukai@users.noreply.github.com>
2020-09-05 00:02:08 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -98,8 +98,8 @@ async def on_member_update(before, after):#Member情報変更時に呼び出し
   
   await channel.send("Ready")
   await channel.send(after.roles)
-  vwau='Role id=739996326737084567 name='vwau'' in after.roles
-  nvwau='Role id=739996326737084566 name='nvwau'' in after.roles
+  vwau="<Role id=739996326737084567 name='vwau'>" in after.roles
+  nvwau="<Role id=739996326737084566 name='nvwau'>" in after.roles
   if vwau:
     await channel.send("vwau")
     if nvwau:
