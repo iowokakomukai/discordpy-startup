5dc4958d2a56e3685b2399204ec02d4751a14988
iowokakomukai <69210735+iowokakomukai@users.noreply.github.com>
2020-09-05 14:13:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -30,11 +30,18 @@ C_ROLE_ID=int(751477451556913253)
 D_ROLE_ID=int(751477456640409625)
 I_ROLE_ID=int(751477458724978818)
 
+#global
+invite_B=0;invite_C=4;invite_D=2
+
 @client.event
 async def on_ready(): #Bot起動準備完了時
   ChannelID = int(739996326909182036) #送信するチャンネルID
   channel = client.get_channel(ChannelID)
   await channel.send("Ready")
+  await channel.send(invite_B)
+  await channel.send(invite_C)
+  await channel.send(invite_D)
+  await channel.send("OK")
 
 @client.event
 async def on_member_join(member):
